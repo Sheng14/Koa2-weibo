11dc8ffe68209e6535c446ccf7e3516ab63e6b02
Sheng14 <1336840941@qq.com>
2020-08-31 15:51:31 +0800

    feat:介绍sequelize-删除和更新

--- sequelize-test/src/select.js
+++ sequelize-test/src/select.js
@@ -1,7 +1,7 @@
 const { Blog, User } = require('./model')
 
 !(async function() {
-    // 查询一条记录（得到一个对象）
+   /* // 查询一条记录（得到一个对象）
     const diguo = await User.findOne({
         where: {
             userName: 'diguo'
@@ -48,7 +48,53 @@ const { Blog, User } = require('./model')
         ]
     })
     console.log(diguoCount.count) // 查询所有记录的总数
-    console.log(diguoCount.rows.map((blog) => blog.dataValues)) // 查询当前页的记录的全部内容
+    console.log(diguoCount.rows.map((blog) => blog.dataValues)) // 查询当前页的记录的全部内容*/
+
+    // 连表查询1 查询blog全部和user的userName与nickName同时符合userNmae为diguo的情况
+    const blogListWithUser = await Blog.findAndCountAll({
+        order: [
+            ['id', 'desc']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['userName', 'nickName'],
+                where: {
+                    userName: 'diguo'
+                }
+            }
+        ]
+    })
+    console.log(blogListWithUser.rows.map((blog => {
+        let blogVal = blog.dataValues
+        blogVal.user = blogVal.user.dataValues // 拿到下面的user的值重新赋给user(因为每条blog肯定是要带有user的信息才能区分是谁嘛)
+        return blogVal
+    })))
+    console.log(blogListWithUser.count) // 2是因为我们限制了查询，只针对diguo进行查询（blog和user都是如此，可以去掉where则全部出来）
+
+    // 连表查询2 查询user全部和blog中的title同时符合id为1的情况
+    const userListWithBlog = await User.findAndCountAll({
+        order: [
+            ['id', 'desc']
+        ],
+        include: [
+            {
+                model: Blog,
+                attributes: ['title'],
+                where: {
+                    userId: 1
+                }
+            }
+        ]
+    })
+    console.log(userListWithBlog.rows.map((user) => {
+        let userVal = user.dataValues
+       // userVal.blog = userVal.blog.dataValues 错误 
+       // 因为user里面不只是一条blog，我这样子做相当于限制死了它只能有一个blog，应该从存放blog的地方blogs数组去找到每一个blog
+       userVal.blogs = userVal.blogs.map((blog) => blog.dataValues)
+        return userVal
+    }))
+    console.log(userListWithBlog.count)
 })()
 // 这个就是负责查询
 
@@ -135,4 +181,49 @@ Executing (default): SELECT `id`, `title`, `content`, `userId`, `createdAt`, `up
     updatedAt: 2020-08-31T05:47:57.000Z
   }
 ]
+
+
+
+Executing (default): SELECT count(`blog`.`id`) AS `count` FROM `blogs` AS `blog` INNER JOIN `users` AS `user` ON `blog`.`userId` = `user`.`id` AND `user`.`userName` 
+= 'diguo';
+Executing (default): SELECT `blog`.`id`, `blog`.`title`, `blog`.`content`, `blog`.`userId`, `blog`.`createdAt`, `blog`.`updatedAt`, `user`.`id` AS `user.id`, `user`.`userName` AS `user.userName`, `user`.`nickName` AS `user.nickName` FROM `blogs` AS `blog` INNER JOIN `users` AS `user` ON `blog`.`userId` = `user`.`id` AND `user`.`userName` = 'diguo' ORDER BY `blog`.`id` DESC;
+[
+  {
+    id: 2,
+    title: '标题2',
+    content: '内容2',
+    userId: 1,
+    createdAt: 2020-08-31T05:47:57.000Z,
+    updatedAt: 2020-08-31T05:47:57.000Z,
+    user: { userName: 'diguo', nickName: '帝国' }
+  },
+  {
+    id: 1,
+    title: '标题1',
+    content: '内容1',
+    userId: 1,
+    createdAt: 2020-08-31T05:47:57.000Z,
+    updatedAt: 2020-08-31T05:47:57.000Z,
+    user: { userName: 'diguo', nickName: '帝国' }
+  }
+]
+2
+
+
+
+
+Executing (default): SELECT count(`user`.`id`) AS `count` FROM `users` AS `user` INNER JOIN `blogs` AS `blogs` ON `user`.`id` = `blogs`.`userId` AND `blogs`.`userId` = 1;
+Executing (default): SELECT `user`.`id`, `user`.`userName`, `user`.`password`, `user`.`nickName`, `user`.`createdAt`, `user`.`updatedAt`, `blogs`.`id` AS `blogs.id`, `blogs`.`title` AS `blogs.title` FROM `users` AS `user` INNER JOIN `blogs` AS `blogs` ON `user`.`id` = `blogs`.`userId` AND `blogs`.`userId` = 1 ORDER BY `user`.`id` DESC;
+[
+  {
+    id: 1,
+    userName: 'diguo',
+    password: '123',
+    nickName: '帝国',
+    createdAt: 2020-08-31T05:47:56.000Z,
+    updatedAt: 2020-08-31T05:47:56.000Z,
+    blogs: [ [Object], [Object] ] // 可以通过在console.log外面加上stringfy展开，这里就不了
+  }
+]
+2
 */
