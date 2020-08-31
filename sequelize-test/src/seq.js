da0bf0f755c632f7ba53d12c012dd22b2f23de16
Sheng14 <1336840941@qq.com>
2020-08-31 17:52:49 +0800

    feat:介绍sequelize-连接池

--- sequelize-test/src/seq.js
+++ sequelize-test/src/seq.js
@@ -5,6 +5,12 @@ const conf = {
     dialect: 'mysql'
 } // 配置项抽离出来
 
+const pool = { // 配置线上连接池环境
+    max: 3, // 最大连接数量
+    min: 0, // 最小连接数量
+    idle: 10000 // 超过10000毫秒没有连接就释放
+}
+
 const seq = new Sequelize('koa2_weibo_db', 'root', '', conf) // 实例化:数据库、用户名、密码、配置
 
 /*seq.authenticate() // 测试连接（返回的是promise）
