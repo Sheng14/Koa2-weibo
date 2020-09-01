3fea8da6eb70da4018a29f5ce60b39bd21b909e6
Sheng14 <1336840941@qq.com>
2020-09-01 15:47:01 +0800

    feat: 介绍redis-node.js操作redis（下）

--- Koa2-weibo/src/cache/_redis.js
+++ Koa2-weibo/src/cache/_redis.js
@@ -13,3 +13,47 @@
  redisClient.on('error', (error) => {
     console.log('redis error', error)
  })
+ 
+/**
+ * 
+ * @param {string} key 键
+ * @param {string} value  值
+ * @param {number} timeout  过期时间 单位为s
+ */
+function set (key, value, timeout = 60*60) {
+    if (typeof value === 'object') { // 如果值是对象我们转化为字符串
+        JSON.stringify(value)
+    }
+    redisClient.set(key, value)
+    redisClient.expire(key, timeout) // 设置过期时间
+}
+
+/**
+ * 
+ * @param {string} key 键
+ */
+function get (key) { // 拿取redis中的键值，node.js从redis中拿数据本质上还是属于IO操作故异步
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            if (val == null) {
+                resolve(null)
+                return
+            }
+            try{
+                resolve(JSON.parse(val)) // 尝试对值进行对象化，因为前面可能字符串化了，当然也可能失败就直接返回即可
+            } catch(ex) {
+                resolve(val)
+            }
+        })
+    })
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+}
