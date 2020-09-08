8fe6a33f8f638d5e5986488778c920e2c6f970a1
Sheng14 <1336840941@qq.com>
2020-09-08 17:49:24 +0800

    feat: 用户设置-创建页面

--- Koa2-weibo/src/routes/view/user.js
+++ Koa2-weibo/src/routes/view/user.js
@@ -4,6 +4,7 @@
  */
 
 const router = require('koa-router')()
+const { loginRedirect } = require('../../middlewares/loginChecks')
 
 /**
  * 获取用户登录信息（为后面判断用户登录状态做准备）
@@ -27,9 +28,14 @@ router.get('/login', async (ctx, next) => {
     await ctx.render('login', getLoginInfo(ctx))
 })
 
-// 注册路由（暂时不传参数）
+// 注册路由 需要传递用户登录与否的信息，好给前端判断并且阻止重复登录/注册
 router.get('/register', async (ctx, next) => {
     await ctx.render('register', getLoginInfo(ctx))
+})
+
+// 用户设置路由
+router.get('/setting', loginRedirect, async (ctx, next) => {
+    await ctx.render('setting', ctx.session.userInfo) // 把用户的信息传过去，因为设置的话肯定会有一些可以被修改的数据
 })
 
 module.exports = router
