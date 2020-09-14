c5110cf0f27742d4cdf7a911100eebd7221b9a8c
Sheng14 <1336840941@qq.com>
2020-09-14 19:52:48 +0800

    test： 创建微博-单元测试及总结

--- Koa2-weibo/test/json.test.js
+++ Koa2-weibo/test/json.test.js
@@ -4,11 +4,19 @@
  */
 
 const server = require('./server')
-
+/*
 test('json接口返回数据格式正确', async () => {
     const res = await server.get('/json')
     expect(res.body).toEqual({
         title: 'koa2 json'
     })
     expect(res.body.title).toBe('koa2 json')
+})*/
+function add (a, b) {
+    return a+b
+}
+
+test('10+20应该等于30', () => {
+    const res = add(10, 20)
+    expect(res).toBe(30)
 })
