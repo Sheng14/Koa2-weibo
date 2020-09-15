a1dc3852b48d7e2d7b7bd704f45c4fd27dd57ca2
Sheng14 <1336840941@qq.com>
2020-09-15 16:49:16 +0800

    feat: 个人主页-开始和创建页面

--- Koa2-weibo/src/routes/view/blog.js
+++ Koa2-weibo/src/routes/view/blog.js
@@ -6,8 +6,22 @@
 const router = require('koa-router')()
 const { loginRedirect } = require('../../middlewares/loginChecks')
 
+// 首页
 router.get('/', loginRedirect, async (ctx, next) => { // 真正的微博首页
     await ctx.render('index', {})
+})
+
+// 个人主页（自己）
+router.get('/profile', loginRedirect, async (ctx, next) => {
+    const { userName } = ctx.session.userInfo // 如果直接搜profile的就拿到存在session的用户名判断是谁再跳转到它的页面
+    ctx.redirect(`/profile/${userName}`)
+})
+
+// 个人主页（动态）
+router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
+    await ctx.render('profile', {
+
+    })
 })
 
 module.exports = router
