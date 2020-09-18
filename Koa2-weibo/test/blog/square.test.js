6cf50df15da20dff81f024c67e068b7eeead4754
Sheng14 <1336840941@qq.com>
2020-09-18 14:21:56 +0800

    feat: 广场页-单元测试及总结

---
+++ Koa2-weibo/test/blog/square.test.js
@@ -0,0 +1,21 @@
+/**
+ * @description 广场测试
+ * @author Sheng14
+ */
+
+const server = require('../server')
+const { COOKIE } = require('../testUserInfo') // 获取测试的常量值用户名和cookie
+
+test('测试广场页，应该成功', async () => {
+    const res = await server
+        .get(`/api/square/loadMore/0`) // 假设测试的是测试用户名的第一页微博数据
+        .set('cookie', COOKIE) // 设置cookie
+    expect(res.body.errno).toBe(0) // 希望这个请求并不会错误
+
+    const data = res.body.data // 测试这个数据是否包含以下所有的属性
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
+})
